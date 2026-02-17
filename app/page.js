65a093fc41930aec8fe95c454a584a0b16bf9cfd
Aravind <gmarav005@gmail.com>
2026-02-17 18:14:35 +0530

    Update css and add components

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
-import Image from "next/image";
-import { dbConnect } from "@/lib/db";
+import ContactForm from "@/components/contact-form";
 
-export default async function Home() {
 
-  await dbConnect();
-
+export default function Home() {
+ 
   return (
-    <div>
-      <h1 className="text-2xl">Hello World</h1>
+   <main className="min-h-screen py-12 px-4">
+    <div className="container mx-auto">
+      <div className="text-center mb-12">
+    <h1 className="text-4xl font-bold mb-4">Server actions Demo</h1>
+    <p className="text-xl text-gray-600 max-2xl mx-auto">Contact form with MongoDb and revalidation</p>
+      </div>
+      <ContactForm/>
     </div>
+   </main>
   );
 }
